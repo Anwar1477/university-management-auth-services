fb4f808bcedbb23256e78491ba158613e9092943
Anwar1477 <joy.cse017@gmail.com>
2023-06-05 16:31:14 +0600

    setup cors parser

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+const port = 3000;
+
+app.use(cors());
+// parse
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("working successfully");
+});
+
+export default app;
