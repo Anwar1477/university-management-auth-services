c7f2c8f8bd9c474f7941c1c9752dccbd40b8c5d7
Anwar1477 <joy.cse017@gmail.com>
2023-06-05 17:24:12 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-const port = 3000;
+import cors from 'cors'
+import express, { Application, Request } from 'express'
+const app: Application = express()
+// const port = 3000
 
-app.use(cors());
+app.use(cors())
 // parse
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("working successfully");
-});
+app.get('/', (req: Request, res: any) => {
+  res.send('working successfully')
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("data base connected");
+    await mongoose.connect(config.database_url as string)
+    console.log('data base connected')
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("failed to connect database");
+    console.log('failed to connect database')
   }
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-main();
+main()
